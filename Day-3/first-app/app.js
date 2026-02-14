aaa1d2919deec710f1aa4b8d483f29ceb02deaea
SrilekhaS20 <lekhaconst.1@gmail.com>
2026-02-14 21:38:53 +0530

    This is test commit to check webhook trigger

--- Day-3/first-app/app.js
+++ Day-3/first-app/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Jenkins CI/CD!\n');
+  res.end('Auto-deployed from GitHub! v2.0\n');
 });
 
 server.listen(3000, () => {
